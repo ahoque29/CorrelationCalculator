9965c58b32d92c2bb64786a9f470aef5ad5502f3
Ashib Hoque <a.hoque29@hotmail.com>
2021-07-15 18:07:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CorrelationCalculator
+{
+	/// <summary>
+	/// Class containing the main entry point into the console application.
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		/// Main entry point into the console application.
+		/// </summary>
+		private static void Main()
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
