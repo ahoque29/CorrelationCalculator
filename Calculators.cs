eb7e49debf3fbdee02cb7a14a96155a299c7a6da
Ashib Hoque <a.hoque29@hotmail.com>
2021-07-17 20:55:41 +0100

    Refactor by moving code to separate methods

---
+++ Calculators.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrelationCalculator
+{
+	public static class Calculators
+	{
+		public static void AdjustRanks(IEnumerable<Sample> samples)
+		{
+			int rank = 1;
+			foreach (var sample in samples.OrderByDescending(s => s.X))
+			{
+				sample.RankX = rank;
+				rank++;
+			}
+
+			foreach (var ranking in samples.OrderByDescending(s => s.X).GroupBy(s => s.X))
+			{
+				double sumOfRank = ranking.Sum(s => s.RankX);
+
+				foreach (var sample in ranking)
+				{
+					sample.RankX = sumOfRank / ranking.Count();
+				}
+			}
+
+			rank = 1;
+			foreach (var sample in samples.OrderByDescending(s => s.Y))
+			{
+				sample.RankY = rank;
+				rank++;
+			}
+
+			foreach (var ranking in samples.OrderByDescending(s => s.Y).GroupBy(s => s.Y))
+			{
+				double sumOfRank = ranking.Sum(s => s.RankY);
+
+				foreach (var sample in ranking)
+				{
+					sample.RankY = sumOfRank / ranking.Count();
+				}
+			}
+		}
+
+		public static double LinearCorrelation(IEnumerable<Sample> samples)
+		{
+			var n = samples.Count();
+			var sumOfX = samples.Sum(s => s.X);
+			var sumOfXSquared = samples.Sum(s => s.X * s.X);
+			var sumOfY = samples.Sum(s => s.Y);
+			var sumOfYSquared = samples.Sum(s => s.Y * s.Y);
+			var sumOfXY = samples.Sum(s => s.X * s.Y);
+
+			return (n * sumOfXY - sumOfX * sumOfY) / Math.Sqrt((n * sumOfXSquared - sumOfX * sumOfX) * (n * sumOfYSquared - sumOfY * sumOfY));
+		}
+
+		public static double SpearmanCorrelation(List<Sample> samples)
+		{
+			var n = samples.Count;
+			return 1 - 6 * samples.Sum(s => Math.Pow((s.RankX - s.RankY), 2)) / (Math.Pow(n, 3) - n);
+		}
+	}
+}
---
+++ FileManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorrelationCalculator
+{
+	/// <summary>
+	/// Class that manages the input file
+	/// </summary>
+	public class FileManager
+	{
+		public string FirstChoice { get; set; }
+		public string SecondChoice { get; set; }
+		public string[] Headers { get; set; }
+		public IEnumerable<string> FileData { get; set; }
+
+
+		public string GetPath()
+		{
+			Console.WriteLine("Please select a file: \n");
+			var fd = new OpenFileDialog();
+			fd.ShowDialog();
+			return fd.FileName;
+		}
+
+		public void SelectColumns(string path)
+		{
+			Headers = File.ReadLines(path)
+				.First()
+				.Split(',');
+
+			Console.WriteLine("Please pick a column from these given options: ");
+			foreach (var column in Headers)
+			{
+				Console.WriteLine(column);
+			}
+			Console.WriteLine();
+			Console.WriteLine("Your first choice: ");
+			FirstChoice = Console.ReadLine().Trim();
+			Console.WriteLine();
+
+			Console.WriteLine("Please pick a column from these remaining options: ");
+			foreach (var column in Headers)
+			{
+				if (column != FirstChoice)
+				{
+					Console.WriteLine(column);
+				}
+			}
+			Console.WriteLine();
+			Console.WriteLine("Your second choice:");
+			SecondChoice = Console.ReadLine().Trim();
+			Console.WriteLine();
+		}
+
+		public IEnumerable<Sample> Sampler(string firstChoice, string secondChoice, string path)
+		{
+			
+			var column1Index = Array.IndexOf(Headers, firstChoice);
+			var column2Index = Array.IndexOf(Headers, secondChoice);
+
+			var samples = new List<Sample>();
+
+			var FileData = File.ReadAllLines(path)
+				.Skip(1)
+				.Where(l => l.Length > 1);
+
+			foreach (var item in FileData)
+			{
+				var entry = item.Split(',');
+
+				samples.Add(new Sample()
+				{
+					X = double.Parse(entry[column1Index]),
+					Y = double.Parse(entry[column2Index])
+				});
+			}
+
+			return samples;
+		}
+	}
+}
